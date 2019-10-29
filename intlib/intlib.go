cec4b1c616c1d24351d08ba2ff15f05405b023b7
fshin1988 <noalv88@gmail.com>
2019-10-29 23:05:33 +0900

    Add gcd and lcm

--- intlib/intlib.go
+++ intlib/intlib.go
@@ -20,3 +20,14 @@ func abs(x int) int {
 	}
 	return -x
 }
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+func lcm(a, b int) int {
+	return a / gcd(a, b) * b
+}
