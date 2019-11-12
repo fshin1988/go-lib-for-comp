dae69a867eadfc084b00efce6e202e2aa2bb69b5
fshin1988 <noalv88@gmail.com>
2019-11-12 22:25:08 +0900

    Add IntHeap

--- container/container.go
+++ container/container.go
@@ -1,4 +1,9 @@
-package container
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
 
 type Stack struct {
 	arr []int
@@ -34,4 +39,32 @@ func (q *Queue) pop() int {
 
 func (q *Queue) isEmpty() bool {
 	return len(q.arr) == 0
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func main() {
+	h := &IntHeap{2, 1, 5}
+	heap.Init(h)
+	heap.Push(h, 3)
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
+	fmt.Println()
 }
