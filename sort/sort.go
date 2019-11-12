188617cd52b25eda7713e3a47ed0a94a37e01835
fshin1988 <noalv88@gmail.com>
2019-11-12 21:05:38 +0900

    Add RuneSlice

--- sort/sort.go
+++ sort/sort.go
@@ -7,3 +7,9 @@ import (
 func reverse(s []int) {
 	sort.Sort(sort.Reverse(sort.IntSlice(s)))
 }
+
+type RuneSlice []rune
+
+func (p RuneSlice) Len() int           { return len(p) }
+func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
