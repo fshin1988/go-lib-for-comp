968cab3c085af110e27a0d2eb4a03ee8b061a1c4
fshin1988 <noalv88@gmail.com>
2019-11-23 15:29:51 +0900

    Add binarySearch

--- sort/sort.go
+++ sort/sort.go
@@ -8,6 +8,20 @@ func reverse(s []int) {
 	sort.Sort(sort.Reverse(sort.IntSlice(s)))
 }
 
+func binarySearch(nums []int, key int) int {
+	left := -1
+	right := len(nums)
+	for right-left > 1 {
+		mid := left + (right-left)/2
+		if nums[mid] >= key {
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+	return right
+}
+
 type RuneSlice []rune
 
 func (p RuneSlice) Len() int           { return len(p) }
