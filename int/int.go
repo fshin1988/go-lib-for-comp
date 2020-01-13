30ece3c2852eb1584201df7f628db8d4ad1d7116
fshin1988 <noalv88@gmail.com>
2020-01-13 14:15:03 +0900

    Add library for combination with high performance

--- int/int.go
+++ int/int.go
@@ -137,3 +137,32 @@ func primeFactors(n int) map[int]int {
 
 	return pfs
 }
+
+// Combination
+const MAX = int(1e+6)
+const MOD = int(1e+9) + 7
+
+var fac [MAX]int
+var finv [MAX]int
+var inv [MAX]int
+
+func COMinit() {
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+	for i := 2; i < MAX; i++ {
+		fac[i] = fac[i-1] * i % MOD
+		inv[i] = MOD - inv[MOD%i]*(MOD/i)%MOD
+		finv[i] = finv[i-1] * inv[i] % MOD
+	}
+}
+
+func COM(n, k int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD
+}
