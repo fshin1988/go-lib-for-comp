06dfa15782f66bf910f14d6888e7b1e26dfa8497
fshin1988 <noalv88@gmail.com>
2020-01-17 19:31:27 +0900

    Fix mod libraries

--- int/int.go
+++ int/int.go
@@ -59,53 +59,6 @@ func toNums(str string) []int {
 	return nums
 }
 
-func modPow(a, n, mod int) int {
-	res := 1
-	for n > 0 {
-		if (n & 1) > 0 {
-			res = res * a % mod
-		}
-		a = a * a % mod
-		n >>= 1
-	}
-	return res
-}
-
-func modComb(big, small, mod int) int {
-	if big < 0 || small < 0 || big < small {
-		return 0
-	}
-	tmp := modFact(big, mod) * modInv(modFact(small, mod), mod) % mod
-	return tmp * modInv(modFact(big-small, mod), mod) % mod
-}
-
-func modFact(a, mod int) int {
-	res := 1
-	for i := a; i > 0; i-- {
-		res *= i
-		res %= mod
-	}
-	return res
-}
-
-func modInv(a, mod int) int {
-	b := mod
-	u := 1
-	v := 0
-	for b > 0 {
-		t := a / b
-		a -= t * b
-		a, b = b, a
-		u -= t * v
-		u, v = v, u
-	}
-	u %= mod
-	if u < 0 {
-		u += mod
-	}
-	return u
-}
-
 func divisor(n int) []int {
 	res := []int{}
 	for i := 1; i*i <= n; i++ {
@@ -138,6 +91,36 @@ func primeFactors(n int) map[int]int {
 	return pfs
 }
 
+func modPow(a, n, mod int) int {
+	res := 1
+	for n > 0 {
+		if (n & 1) > 0 {
+			res = res * a % mod
+		}
+		a = a * a % mod
+		n >>= 1
+	}
+	return res
+}
+
+func modInv(a, mod int) int {
+	b := mod
+	u := 1
+	v := 0
+	for b > 0 {
+		t := a / b
+		a -= t * b
+		a, b = b, a
+		u -= t * v
+		u, v = v, u
+	}
+	u %= mod
+	if u < 0 {
+		u += mod
+	}
+	return u
+}
+
 // Combination
 const MAX = int(1e+6)
 const MOD = int(1e+9) + 7
@@ -146,7 +129,7 @@ var fac [MAX]int
 var finv [MAX]int
 var inv [MAX]int
 
-func COMinit() {
+func initComb() {
 	fac[0], fac[1] = 1, 1
 	finv[0], finv[1] = 1, 1
 	inv[1] = 1
@@ -157,7 +140,7 @@ func COMinit() {
 	}
 }
 
-func COM(n, k int) int {
+func modComb(n, k int) int {
 	if n < k {
 		return 0
 	}
