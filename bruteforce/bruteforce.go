709e8fbdf50edef0db35ca7c8732c1b557099fea
fshin1988 <noalv88@gmail.com>
2020-02-04 18:50:07 +0900

    Add nextPermutation

--- bruteforce/bruteforce.go
+++ bruteforce/bruteforce.go
@@ -39,3 +39,27 @@ func permutations(nums []int, n int) {
 		seen[v] = false
 	}
 }
+
+func nextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
